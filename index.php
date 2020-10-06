ce508d71ec4a4546193b9e39ad1e701b4c46dc80
Joe Reynolds <joereynolds952@gmail.com>
2020-10-06 17:03:19 +0100

    Add example index.php

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GearForMusic\Batch\Batch;
+use GearForMusic\Consignment\Consignment;
+use GearForMusic\Courier\Anc;
+use GearForMusic\Courier\RoyalMail;
+
+$royalMailConsignment = new Consignment(new RoyalMail());
+
+$batch = new Batch([$royalMailConsignment]);
+
+$batch->addConsignment(new Consignment(new Anc()));
+
+$batch->start();
+$batch->end();
